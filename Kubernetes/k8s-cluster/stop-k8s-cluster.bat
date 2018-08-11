7f38283f34770b9384084fbef7e240c47501f9b3
jsa4000 <jsa4000@gmail.com>
2018-08-11 16:22:53 +0200

    Updated some documents

---
+++ Kubernetes/k8s-cluster/stop-k8s-cluster.bat
@@ -0,0 +1,21 @@
+@echo off
+
+ECHO Stopping Kubernetes cluster
+ECHO ---
+ECHO Stop K8s Master Server
+
+cd k8s-master
+vagrant halt
+cd ..
+
+ECHO Stop K8s Node 01 Server
+
+cd k8s-node01
+vagrant halt
+cd ..
+
+ECHO Stop K8s Node 02 Server
+
+cd k8s-node02
+vagrant halt
+cd ..
--- Kubernetes/samples/kubectl-console/nodejs-sample/src/server.js
+++ Kubernetes/samples/kubectl-console/nodejs-sample/src/server.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var os = require("os");
 
 var handleRequest = function(request, response) {
+  var hostname = os.hostname();
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('Hello World! <br> HOSTNAME:' + hostname);
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
