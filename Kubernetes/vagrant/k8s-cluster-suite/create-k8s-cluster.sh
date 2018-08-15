c561aa9709446bc1d4e70adafd129ce104611138
jsa4000 <jsa4000@gmail.com>
2018-08-15 18:19:20 +0200

    added load dashboard automatically

--- Kubernetes/vagrant/k8s-cluster-suite/create-k8s-cluster.sh
+++ Kubernetes/vagrant/k8s-cluster-suite/create-k8s-cluster.sh
@@ -4,16 +4,14 @@ echo "Create Kubernetes Cluster (KVM)"
 echo "- Loading the entire kubernetes cluster"
 vagrant up
 
-echo "- Performing the post process"
+echo "- Performing the post-process"
 vagrant ssh k8s-master -c "sudo bash /vagrant/post-provision/all-install.sh"
 
 echo "- Process Finished. You can acces to the following nodes via ssh on ansible"
 echo ""
 vagrant status
-echo ""
-echo "- To run the dahsboard use the following command on k8s-master"
-echo ""
-echo "       sudo kubectl proxy --port=9999 --address=10.0.0.11 --accept-hosts='^*$' $" 
+
+vagrant ssh k8s-master -c "sudo kubectl proxy --port=9999 --address=10.0.0.11 --accept-hosts='^*$' &"
 echo ""
 echo "- Following is the link to access to the dashboard:"
 echo ""
