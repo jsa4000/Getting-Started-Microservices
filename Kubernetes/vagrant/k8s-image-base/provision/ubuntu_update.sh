1e0cdec533966a5ef2592c1d668b6fa3abca6161
Javier Santos <jsa4000@gmail.com>
2018-08-21 11:20:31 +0200

    Removed daily updates generating the Ubuntu base image

--- Kubernetes/vagrant/k8s-image-base/provision/ubuntu_update.sh
+++ Kubernetes/vagrant/k8s-image-base/provision/ubuntu_update.sh
@@ -2,3 +2,7 @@
 echo "- Updating Linux Distribution"
 apt-get update -y
 apt-get upgrade -y
+
+echo "- Disable unwanted updates"
+systemctl disable apt-daily.service
+systemctl disable apt-daily.timer
