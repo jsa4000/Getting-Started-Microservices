eb57f025072e3f5b1b1536971a9f29706168193d
jsa4000 <jsa4000@gmail.com>
2018-11-02 16:22:26 +0100

    Added jib gradle plugin to generate docker images

--- event-driven/rabbitmq/device-stub/build.sh
+++ event-driven/rabbitmq/device-stub/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-# Build project and create docker image
-gradle builDockerLocal
+# Build project and create Dockerfile
+gradle jibExportDockerContext
+
+# Build the docker image
+docker build ./build/jib-docker-context -t devices-stub:latest
