6d2ba0b3b993f640cc3a58d0a2847cadde799d5e
javier.santos <javier.santos@fon.es>
2018-11-26 11:20:37 +0100

    Added specific versions to docker and kubernetes

--- devops/Kubernetes/vagrant/k8s-image-base/provision/docker_install.sh
+++ devops/Kubernetes/vagrant/k8s-image-base/provision/docker_install.sh
@@ -6,10 +6,26 @@ apt-get install \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+# Latest Docker Version
+
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+#apt-get update -y
+#apt-get install docker-ce -y
+
+# Oldest Docker Version
+
+curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
+apt-add-repository "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main"
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get update -y
-apt-get install docker-ce -y
+apt-get install docker-ce=17.06.0~ce-0~ubuntu -y --allow-unauthenticated
+
+
--- devops/Kubernetes/vagrant/k8s-image-base/provision/kubeadm_install.sh
+++ devops/Kubernetes/vagrant/k8s-image-base/provision/kubeadm_install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 echo "- Installing Kubeadm"
 apt-get update && apt-get install -y apt-transport-https curl
+
+# Custom Kubernetes version
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-chattr -i /etc/apt/sources.list.d/kubernetes.list
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update -y
+apt-get install -y kubelet=1.11.4-00 kubectl=1.11.4-00 kubeadm=1.11.4-00
 
