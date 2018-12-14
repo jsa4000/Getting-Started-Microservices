4cb0c24298711030221c9cc21a46e806ec3e1109
javier.santos <javier.santos@fon.es>
2018-12-14 09:17:22 +0100

    Updated User Controller Filter to reject the incoming requests depending on the resources

--- patterns/api-gateway/src/management/src/main/java/com/example/management/config/security/JwtUserControllerFilter.java
+++ patterns/api-gateway/src/management/src/main/java/com/example/management/config/security/JwtUserControllerFilter.java
@@ -6,20 +6,26 @@ import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.security.access.AuthorizationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
+@SuppressWarnings("unchecked")
 public class JwtUserControllerFilter implements   Filter  {
 
     private static final String TOKEN_USER_NAME = "user_name";
     private static final String TOKEN_TOKEN_PREFIX = "Bearer ";
     private static final String TOKEN_AUTHORIZATION_HEADER = "Authorization";
     private static final String TOKEN_RESOURCES_PARAMETER = "resources";
+    private static final String TOKEN_AUTHORITIES_PARAMETER = "authorities";
+    private static final String TOKEN_ADMIN_AUTHORITY_ROLE = "ROLE_ADMIN";
 
     private SecurityProperties securityProperties;
 
@@ -38,24 +44,21 @@ public class JwtUserControllerFilter implements   Filter  {
             chain.doFilter(request, response);
             return;
         }
-        String token = header.replace(TOKEN_TOKEN_PREFIX, StringUtils.EMPTY);
-        try {
-            Claims claims = Jwts.parser()
-                    .setSigningKey(securityProperties.getSymmetricKey().getBytes())
-                    .parseClaimsJws(token)
-                    .getBody();
 
-            if(claims.containsKey(TOKEN_USER_NAME)) {
-                @SuppressWarnings("unchecked")
-                List<String> resources = (List<String>) claims.get(TOKEN_RESOURCES_PARAMETER);
-                if (!resources.stream().anyMatch(x->x.equals(request.getRequestURI()))) {
-                    throw new AuthorizationServiceException(String.format("User %s not authorized for resource %s",
-                                    claims.get(TOKEN_USER_NAME), request.getRequestURI()));
-                }
+        Claims claims = Jwts.parser()
+                .setSigningKey(securityProperties.getSymmetricKey().getBytes())
+                .parseClaimsJws(header.replace(TOKEN_TOKEN_PREFIX, StringUtils.EMPTY))
+                .getBody();
+
+        List<String> authorities = (List<String>) claims.get(TOKEN_AUTHORITIES_PARAMETER);
+        if (!authorities.stream().anyMatch(x -> x.equals(TOKEN_ADMIN_AUTHORITY_ROLE))) {
+
+
+            List<String> resources = (List<String>) claims.get(TOKEN_RESOURCES_PARAMETER);
+            if (!resources.stream().anyMatch(x -> x.equals(request.getRequestURI()))) {
+                throw new AuthorizationServiceException(String.format("User %s not authorized for resource %s",
+                        claims.get(TOKEN_USER_NAME), request.getRequestURI()));
             }
-        } catch (Exception e) {
-            //SecurityContextHolder.clearContext();
-            log.error(e.getMessage());
         }
         chain.doFilter(request, response);
     }
