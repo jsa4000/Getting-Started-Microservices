40800190633d97edc228166eedc77e0f192bf566
jsa4000 <jsa4000@gmail.com>
2018-12-18 23:24:07 +0100

    Small changes in the openstack configureation

--- devops/Terraform/vagrant/openstack-master/provision/openstack_install.sh
+++ devops/Terraform/vagrant/openstack-master/provision/openstack_install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 echo "- Installing OpenStack"
 
+#sudo su
 useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+#su -l stack
 cd /opt/stack
 git clone https://git.openstack.org/openstack-dev/devstack
+cd devstack
 cp /vagrant/files/local.conf local.conf
 
 # Finally execute the script to install openstack
