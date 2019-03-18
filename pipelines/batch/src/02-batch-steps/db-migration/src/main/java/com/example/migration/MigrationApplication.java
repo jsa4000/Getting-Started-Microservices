6094bcffa850805309a9f84997ce68e91db5e62d
Santos Andrés <jsantosa@minsait.com>
2019-03-18 11:27:32 +0100

    Changed migration spring tool to command line

--- pipelines/batch/src/02-batch-steps/db-migration/src/main/java/com/example/migration/MigrationApplication.java
+++ pipelines/batch/src/02-batch-steps/db-migration/src/main/java/com/example/migration/MigrationApplication.java
@@ -1,13 +1,18 @@
 package com.example.migration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MigrationApplication {
+    private static final Logger log = LoggerFactory.getLogger(MigrationApplication.class);
 
     public static void main(String[] args) {
+        log.info("Starting migration...");
         SpringApplication.run(MigrationApplication.class, args);
+        log.info("Migration has finished.");
     }
 
 }
