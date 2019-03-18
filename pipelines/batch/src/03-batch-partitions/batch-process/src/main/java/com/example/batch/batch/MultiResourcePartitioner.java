e3683b95430578a2eb4697a102ae1a8722a232fb
Santos Andrés <jsantosa@minsait.com>
2019-03-18 16:11:24 +0100

    Finished partitioner example using Spring Batch

--- pipelines/batch/src/03-batch-partitions/batch-process/src/main/java/com/example/batch/batch/MultiResourcePartitioner.java
+++ pipelines/batch/src/03-batch-partitions/batch-process/src/main/java/com/example/batch/batch/MultiResourcePartitioner.java
@@ -3,39 +3,45 @@ package com.example.batch.batch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.ExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-public class CustomMultiResourcePartitioner implements Partitioner {
+@Component
+public class MultiResourcePartitioner implements Partitioner {
 
-    private static final String DEFAULT_KEY_NAME = "fileName";
+    private static final String FILENAME_KEY = "fileName";
     private static final String PARTITION_KEY = "partition";
 
-    private Resource[] resources = new Resource[0];
-    private String keyName = DEFAULT_KEY_NAME;
+    @Autowired
+    ResourcePatternResolver resourcePatternResolver;
 
-    public void setResources(Resource[] resources) {
-        this.resources = resources;
-    }
-
-    public void setKeyName(String keyName) {
-        this.keyName = keyName;
-    }
+    @Value("${batch.pattern}")
+    String pattern;
 
     @Override
     public Map<String, ExecutionContext> partition(int gridSize) {
+        Resource[] resources;
+        try {
+            resources = resourcePatternResolver.getResources("file:" + pattern);
+            log.info("Current files to process are: " + resources.length);
+        } catch (IOException e) {
+            throw new RuntimeException("I/O problems when resolving" + " the input file pattern.", e);
+        }
         Map<String, ExecutionContext> map = new HashMap<>(gridSize);
-        int i = 0, k = 1;
+        int i = 0;
         for (Resource resource : resources) {
             try {
                 ExecutionContext context = new ExecutionContext();
                 log.info("Adding " + resource.getFile().getAbsolutePath() + " file to partition");
-                context.putString(keyName, "file:" + resource.getFile().getAbsolutePath());
-                context.putString("opFileName", "output" + k++ + ".xml");
+                context.putString(FILENAME_KEY, "file:" + resource.getFile().getAbsolutePath());
                 map.put(PARTITION_KEY + i, context);
                 i++;
             } catch (IOException ex) {
--- pipelines/batch/src/03-batch-partitions/batch-process/src/main/java/com/example/batch/config/MasterConfiguration.java
+++ pipelines/batch/src/03-batch-partitions/batch-process/src/main/java/com/example/batch/config/MasterConfiguration.java
@@ -1,6 +1,5 @@
 package com.example.batch.config;
 
-import com.example.batch.batch.CustomMultiResourcePartitioner;
 import com.example.batch.batch.PostProcessTasklet;
 import com.example.batch.batch.PreProcessTasklet;
 import com.example.batch.listener.JobCompletionListener;
@@ -10,20 +9,15 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.io.IOException;
 
 @Slf4j
 @EnableRetry
@@ -34,32 +28,13 @@ public class MasterConfiguration {
     @Value("${batch.max-threads:4}")
     private int maxThreads;
 
-    @Autowired
-    ResourcePatternResolver resoursePatternResolver;
-
-    @Bean
-    @StepScope
-    public CustomMultiResourcePartitioner partitioner(
-            @Value("${batch.pattern}") String pattern) {
-        CustomMultiResourcePartitioner partitioner = new CustomMultiResourcePartitioner();
-        try {
-            Resource[] resources = resoursePatternResolver.getResources("file:" + pattern);
-            log.info("Current files to process are: " + resources.length);
-            partitioner.setResources(resources);
-            //partitioner.partition(5);
-        } catch (IOException e) {
-            throw new RuntimeException("I/O problems when resolving"
-                    + " the input file pattern.", e);
-        }
-        return partitioner;
-    }
-
     @Bean
     public Step partitionStep(StepBuilderFactory stepBuilderFactory,
+                              Partitioner partitioner,
                               @Qualifier("slaveStep") Step slaveStep,
                               @Qualifier("masterTaskExecutor") TaskExecutor taskExecutor) {
         return stepBuilderFactory.get("partitionStep")
-                .partitioner("slaveStep", partitioner(null))
+                .partitioner("slaveStep", partitioner)
                 .step(slaveStep)
                 .taskExecutor(taskExecutor)
                 .build();
@@ -86,9 +61,9 @@ public class MasterConfiguration {
         return jobBuilderFactory.get("job")
                 .incrementer(new RunIdIncrementer())
                 .listener(listener)
-                //.start(preProcessStep(null,null))
-                .start(partitionStep(null, null, null))
-                //.next(postProcessStep(null,null))
+                .start(preProcessStep(null,null))
+                .next(partitionStep(null, null, null, null))
+                .next(postProcessStep(null,null))
                 .build();
     }
 
