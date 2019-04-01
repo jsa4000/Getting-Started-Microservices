72650b2b890e9dde337e939b7e57c342a93a46f0
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2019-04-01 10:40:30 +0200

    Added parameters to csv generator

--- pipelines/batch/src/06-batch-dataflow-k8s/csv-generator/src/main/java/com/example/generator/Application.java
+++ pipelines/batch/src/06-batch-dataflow-k8s/csv-generator/src/main/java/com/example/generator/Application.java
@@ -3,6 +3,7 @@ package com.example.generator;
 import com.example.generator.writer.CsvWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,23 +19,35 @@ public class Application implements CommandLineRunner {
     }
 
     @Autowired
-    CsvWriter writer;
+    private CsvWriter writer;
 
-    @Override
-    public void run(String... args){
-        log.info("Starting CSV Generator");
+    @Value("${pathString:/tmp/bigfile.csv}")
+    private String pathString;
 
-        String pathString = "/tmp/bigfile.csv";
-        long count = 5000;
+    @Value("${count:1000000}")
+    private long count;
 
+    public void benchmark(long count) {
         try {
             for (int i = 0; i < 3; i ++) {
-                writer.writeOpenCsv(Paths.get(pathString),count);
+                writer.writeOpenCsv(Paths.get("/tmp/bigfile.csv"),count);
 
                 writer.writeCsv(Paths.get("/tmp/bigfile1.csv"),count);
 
                 writer.writeCsvFromBean(Paths.get("/tmp/bigfile2.csv"),count);
             }
+        } catch (Throwable ex) {
+            log.error("Error during the generation of the csv file",ex);
+        }
+    }
+
+    @Override
+    public void run(String... args){
+        log.info("Starting CSV Generator");
+
+        try {
+            writer.writeOpenCsv(Paths.get(pathString),count);
+
         } catch (Throwable ex) {
             log.error("Error during the generation of the csv file",ex);
         }
