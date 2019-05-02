6bb9351b8ef315d542708db6975463edb8946b85
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2019-05-02 21:13:16 +0200

    Generated minimal project with api-first development and asciidoc

---
+++ strategies/api-first-develomnet/src/src/main/java/com/example/micro/MicroApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroApplication.class, args);
+	}
+
+}
---
+++ strategies/api-first-develomnet/src/src/test/java/com/example/micro/MicroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.micro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
