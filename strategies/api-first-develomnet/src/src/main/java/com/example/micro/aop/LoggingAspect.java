c64a31a07cdb1fd54df7e43e177a17096780cbe1
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2019-05-03 17:26:05 +0200

    Modified jhipster to create service implmentation and interfaces separately

--- strategies/api-first-develomnet/src/src/main/java/com/example/micro/aop/LoggingAspect.java
+++ strategies/api-first-develomnet/src/src/main/java/com/example/micro/aop/LoggingAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
